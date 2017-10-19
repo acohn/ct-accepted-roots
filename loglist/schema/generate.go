be23968febabcaaef2ac5489f567fffe57b20b9f
Alex Cohn <alex@alexcohn.com>
2017-10-18 19:10:10 -0500

    don't actually generate the log JSON structs

--- loglist/schema/generate.go
+++
@@ -1,5 +0,0 @@
-// Install schema-generate first: go get github.com/a-h/generate/cmd/schema-generate
-// Latest schema is at  https://www.gstatic.com/ct/log_list/log_list_schema.json
-//go:generate schema-generate -i log_list_schema.json -o schema.go -p schema
-
-package schema
