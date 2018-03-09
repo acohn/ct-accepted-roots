8317ea0466681be4e03573b76f55bb75afdd7b00
Alex Cohn <alex@alexcohn.com>
2018-03-09 16:03:49 -0600

    add support for prefix and postfix logs to cover certs outside of the sharded log's window

--- shardedconfigs/main.go
+++ shardedconfigs/main.go
@@ -1,34 +1,87 @@
 package main
 
 import (
-	"github.com/acohn/ct-accepted-roots/loglist"
-	"github.com/gogo/protobuf/proto"
-	pb_ts "github.com/golang/protobuf/ptypes/timestamp"
-	"github.com/google/certificate-transparency-go/client/configpb"
+	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"regexp"
 	"sort"
 	"strconv"
+	"strings"
 	"time"
+
+	"github.com/acohn/ct-accepted-roots/loglist"
+	"github.com/gogo/protobuf/proto"
+	pb_ts "github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/google/certificate-transparency-go/client/configpb"
 )
 
-func main() {
-	var regex *regexp.Regexp
-	if len(os.Args) < 2 {
-		log.Fatal("Must specify name of sharded log, either argon, nessie, nimbus, or yeti")
+var supportedShards = map[ShardedLogName]*regexp.Regexp{
+	"nimbus": regexp.MustCompile(`^ct\.cloudflare\.com/logs/nimbus([0-9]{4})$`),
+	"yeti":   regexp.MustCompile(`^yeti([0-9]{4})\.ct\.digicert\.com/log$`),
+	"nessie": regexp.MustCompile(`^nessie([0-9]{4})\.ct\.digicert\.com/log$`),
+	"argon":  regexp.MustCompile(`^ct\.googleapis\.com/logs/argon([0-9]{4})$`),
+}
+
+type ShardedLogName string
+
+func (s *ShardedLogName) String() string {
+	return string(*s)
+}
+
+func (s *ShardedLogName) Set(name string) error {
+	_, ok := supportedShards[ShardedLogName(name)]
+	if ok {
+		*s = ShardedLogName(name)
+		return nil
+	} else {
+		return fmt.Errorf("Unknown shard name %s", name)
 	}
-	switch logName := os.Args[1]; logName {
-	case "nimbus":
-		regex = regexp.MustCompile(`^ct\.cloudflare\.com/logs/nimbus([0-9]{4})$`)
-	case "yeti":
-		regex = regexp.MustCompile(`^yeti([0-9]{4})\.ct\.digicert\.com/log$`)
-case "nessie":
-                regex = regexp.MustCompile(`^nessie([0-9]{4})\.ct\.digicert\.com/log$`)
-	case "argon":
-		regex = regexp.MustCompile(`^ct\.googleapis\.com/logs/argon([0-9]{4})$`)
-	default:
-		log.Fatal("Unknown sharded log. Choose one of argon, nessie, nimbus, or yeti")
+}
+
+type LogUrl string
+
+func (u *LogUrl) String() string {
+	return string(*u)
+}
+func (u *LogUrl) Set(log string) error {
+	_, err := loglist.ByLogURL(log)
+	if err != nil {
+		return err
+	}
+	*u = LogUrl(log)
+	return nil
+}
+
+var options = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+var shardedLog ShardedLogName
+var fileName string
+var prefixLog LogUrl
+var postfixLog LogUrl
+
+func init() {
+	shardNames := make([]string, 0, len(supportedShards))
+	for name := range supportedShards {
+		shardNames = append(shardNames, string(name))
+	}
+	sort.Strings(shardNames)
+	shards := strings.Join(shardNames, ", ")
+
+	options.Var(&shardedLog, "l", "Name of `shard`ed log; one of "+shards)
+	options.StringVar(&fileName, "o", "", "`file` to output log config to (default stdout)")
+	options.Var(&prefixLog, "prefix", "non-sharded log `url` for certificates that expire before the earliest shard")
+	options.Var(&postfixLog, "postfix", "non-sharded log `url` for certificates that expire after the last shard")
+}
+
+func main() {
+	options.Parse(os.Args[1:])
+	regex, ok := supportedShards[shardedLog]
+	if !ok {
+		options.Usage()
+		os.Exit(1)
 	}
 	config := new(configpb.TemporalLogConfig)
 
@@ -57,6 +110,56 @@ case "nessie":
 		return config.Shard[i].NotAfterStart.Seconds < config.Shard[j].NotAfterStart.Seconds
 	})
 
-	proto.MarshalText(os.Stdout, config)
+	if prefixLog != "" {
+		prefix, err := loglist.ByLogURL(string(prefixLog))
+		if err != nil {
+			log.Fatalf("Couldn't find prefix log: %v", err)
+		}
 
+		prefixConfig := new(configpb.LogShardConfig)
+		prefixConfig.Uri = "https://" + prefix.Url
+
+		prefixConfig.PublicKeyDer, err = prefix.KeyDER()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		prefixConfig.NotAfterStart = nil
+		prefixConfig.NotAfterLimit = config.Shard[0].NotAfterStart
+
+		config.Shard = append([]*configpb.LogShardConfig{prefixConfig}, config.Shard...)
+	}
+
+	if postfixLog != "" {
+		postfix, err := loglist.ByLogURL(string(postfixLog))
+		if err != nil {
+			log.Fatal(err)
+		}
+		postfixConfig := new(configpb.LogShardConfig)
+		postfixConfig.Uri = "https://" + postfix.Url
+
+		postfixConfig.PublicKeyDer, err = postfix.KeyDER()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		postfixConfig.NotAfterStart = config.Shard[len(config.Shard)-1].NotAfterLimit
+		postfixConfig.NotAfterLimit = nil
+
+		config.Shard = append(config.Shard, postfixConfig)
+	}
+	var output io.Writer
+	if fileName != "" {
+		var err error
+		output, err = os.Create(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		output = os.Stdout
+	}
+	proto.MarshalText(output, config)
+	if closer, ok := output.(io.Closer); ok {
+		closer.Close()
+	}
 }
